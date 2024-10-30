f555808fa56cc28f687e5da689b6d2f218b0a480
32Valik92 <strukalo.valentyn@gmail.com>
2024-10-30 21:58:16 +0200

    #FFD012 when we are only in "/"

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,11 +1,13 @@
 import "./HeaderComponent.css";
 
 import React, {FC} from "react";
+import {Link, useLocation} from "react-router-dom";
 
 import {useAppSelector} from "../../redux";
 
 const HeaderComponent: FC = () => {
    const {buyingBooks} = useAppSelector(state => state.bookStore);
+   const location = useLocation();
 
    return (
       <header className="header-component">
@@ -17,14 +19,16 @@ const HeaderComponent: FC = () => {
 
             <div className="links-container">
                <div className="link link-home">Home</div>
-               <div className="link link-shop">Shop</div>
+               <div className={`link link-shop ${location.pathname === "/" ? "highlighted" : ""}`}>Shop</div>
                <div className="link link-about">About</div>
                <div className="link link-contact">Contact</div>
             </div>
 
-            <div className={`buy-icon ${buyingBooks.length > 0 ? "active" : ""}`}>
-               <img src="/images/Vector.svg" alt="Vector"/>
-            </div>
+            <Link to={"shopping-cart"}>
+               <div className={`buy-icon ${buyingBooks.length > 0 ? "active" : ""}`}>
+                  <img src="/images/Vector.svg" alt="Vector"/>
+               </div>
+            </Link>
          </div>
       </header>
    );
