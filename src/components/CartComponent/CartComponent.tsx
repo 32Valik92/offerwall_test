f33c459ad9dc7d0f64823982c56cd374bc92ceed
32Valik92 <strukalo.valentyn@gmail.com>
2024-10-31 18:40:05 +0200

    cart mobile

--- src/components/CartComponent/CartComponent.tsx
+++ src/components/CartComponent/CartComponent.tsx
@@ -25,32 +25,35 @@ const CartComponent: FC<IProps> = ({book}) => {
          return newCount;
       });
    };
-   
+
    const incFun = () => {
       setCount(prevState => prevState + 1);
       dispatch(bookActions.changeTotalSum(book.price));
 
    };
-    
+
    return (
       <div key={book.id} className="buy-book-item">
          <div className="book">
             <div className="buy-book-img"><img src={book.imageURL} alt={book.title}/></div>
 
-            <div className="book-title">{book.title}</div>
+            <div className="book-info">
+               <div className="book-title mobile-width">{book.title}</div>
 
-            <div className="book-price">${book.price}</div>
+               <div className="book-price mobile-width">${book.price}</div>
 
-            <div className="book-count">
-               <span onClick={() => decFun(book.id, book.price)}>-</span>
-               <span className="count">{count}</span>
-               <span onClick={() => incFun()}>+</span>
+               <div className="book-count mobile-width">
+                  <span onClick={() => decFun(book.id, book.price)}>-</span>
+                  <span className="count">{count}</span>
+                  <span onClick={() => incFun()}>+</span>
+               </div>
+
+               <div className="book-price mobile-width">${(book.price * count).toFixed(2)}</div>
             </div>
-
-            <div className="book-price">${(book.price * count).toFixed(2)}</div>
          </div>
 
-         <div className="trash" onClick={() => dispatch(bookActions.removeBookFromCart(book.id))}><img src="/images/trash.svg" alt="trash"/></div>
+         <div className="trash" onClick={() => dispatch(bookActions.removeBookFromCart(book.id))}><img
+            src="/images/trash.svg" alt="trash"/></div>
       </div>
    );
 };
